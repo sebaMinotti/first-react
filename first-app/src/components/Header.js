bc1183e95e836cd8546721457edc910b623fca39
sebaMinotti <seba.minotti@gmail.com>
2024-01-12 16:01:42 +0100

    prima app con react

---
+++ first-app/src/components/Header.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import '../style/header.css'
+function Header() {
+  return (
+      <header>
+            <h1>My first react component 😊</h1>
+      </header>
+  )
+}
+
+export default Header
